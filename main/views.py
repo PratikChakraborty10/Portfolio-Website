44e65ce4daba51759aa15ded8f91991dd9562ffe
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2021-05-07 00:19:27 +0530

    basic static structure added

--- main/views.py
+++ main/views.py
@@ -6,3 +6,10 @@ def index(request):
     context = {}
     return render(request, 'main/index.html', context)
 
+def projects(request):
+    context = {}
+    return render(request, 'main/projects.html', context)
+
+def languages(request):
+    context ={}
+    return render(request, 'main/languages.html', context)
